52558797f5e7be4d288a327a8ca3e55a11e0717a
Mattias Wikström <fyrkant@gmail.com>
2022-06-02 21:27:24 +0200

    Start fetching some data

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,25 +1,57 @@
 import { useLoaderData, Outlet, LoaderFunction, useSearchParams } from 'react-router-dom';
 
+const gql = `
+query SearchRespository($query: String!) {
+  search(query: $query, type: REPOSITORY, first: 20) {
+    edges {
+      node {
+        ... on Repository {
+          id
+          name
+          forkCount
+          stargazerCount
+          languages(first: 3) {
+            edges {
+              node {
+                name
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`;
+
 export const loader: LoaderFunction = async ({ request }) => {
   const url = new URL(request.url);
-  const searchTerm = url.searchParams.get('q');
+  const query = url.searchParams.get('query');
 
-  console.log(searchTerm);
-  return Promise.resolve([
-    {
-      id: 'abc',
-      title: 'Fake Note',
-      content: "We'll replace this with real data soon",
+  return fetch('https://api.github.com/graphql', {
+    method: 'POST',
+    headers: {
+      Authorization: `bearer ${import.meta.env.VITE_GITHUB_ACCESS_TOKEN}`,
+      'Content-Type': 'application/json',
     },
-  ]);
+    body: JSON.stringify({ variables: { query }, query: gql }),
+  });
 };
 
 export default function Root() {
-  // const notes = useLoaderData();
+  const data = useLoaderData();
   const [searchParams, setSearchParams] = useSearchParams();
 
+  console.log(data);
+
   return (
-    <div>
+    <main className="container">
+      <h1>Country Search</h1>
+      <header />
+
+      <div className="search-bar">
+        <input />
+      </div>
       <button
         type="button"
         onClick={() => {
@@ -30,6 +62,6 @@ export default function Root() {
       </button>
 
       <Outlet />
-    </div>
+    </main>
   );
 }
